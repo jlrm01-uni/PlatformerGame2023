84e59cd622ec3336c00936870e0ec559165b0557
Jorge Rodriguez <jorge.rodriguez@guayama.inter.edu>
2023-09-19 21:07:32 -0400

    Coin collection added

---
+++ scripts/coin.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		print("Collected a coin!")
+		queue_free()
+		GameState.current_coins += 1
+		print(GameState.current_coins)
